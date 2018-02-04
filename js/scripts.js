6f26fe4952a4e93f6eb6e8c6758d68b39c0e9ff9
Slavcho Mitrov <slavemitrov@yahoo.com>
2018-02-04 19:07:06 +0100

    Add branching in jquery function to display a warning message if user enters a number bigger than 250

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //creates array 'numbers' and pushes inside the numbers
 //from one up to the inputed number
-var testFunction = function(number) {
+var numbers = function(number) {
   var numbers = [];
   for (var i = 1; i <= number; i++) {
     numbers.push(i);
@@ -12,22 +12,29 @@ $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var number = $("#number").val();
-    var numbers = testFunction(number);
+    var userInput = parseInt($("#number").val());
+    var result = numbers(userInput);
 
     //displays every number to the user in a list
     $("#result").show();
     $("#result ul").empty();
-    numbers.forEach(function(number) {
-      if ((number % 3 === 0) && (number % 5 !== 0)) {
-        $("#result ul").prepend("<li>ping</li>");
-      } else if ((number % 5 === 0) && (number % 3 !== 0)) {
-        $("#result ul").prepend("<li>pong</li>");
-      } else if ((number % 3 === 0) && (number % 5 === 0)) {
-        $("#result ul").prepend("<li>ping pong</li>");
-      } else {
-        $("#result ul").prepend("<li>" + number + "</li>");
-      }
-    });
+
+    if (userInput > 250) {
+      var confirmation = confirm("Entering a number bigger than 250 may cause the page to crash. Are you sure you want to continue?");
+    }
+
+    if (confirmation || (userInput <= 250)) {
+      result.forEach(function(number) {
+        if ((number % 3 === 0) && (number % 5 !== 0)) {
+          $("#result ul").prepend("<li>ping</li>");
+        } else if ((number % 5 === 0) && (number % 3 !== 0)) {
+          $("#result ul").prepend("<li>pong</li>");
+        } else if ((number % 3 === 0) && (number % 5 === 0)) {
+          $("#result ul").prepend("<li>ping pong</li>");
+        } else {
+          $("#result ul").prepend("<li>" + number + "</li>");
+        }
+      });
+    }
   });
 });
